3918c54833ee915db3639a1433ea2b9ff8c1a4d2
Laila Ibrahim <laila457@h-eng.helwan.edu.eg>
2021-07-14 00:50:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+require "header.php";
+
+
+?>
+<main>
+   <?php
+   if(isset($_SESSION['userId'])){
+       echo '<p>You are Logged in!</p>';
+   }else{
+       echo '<p>You are Logged out!</p>';
+       
+   }
+ 
+?>
+
+  
+    
+</main>
+
+<?php
+
+require "footer.php";
+?>
